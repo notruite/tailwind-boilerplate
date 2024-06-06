0785788469c1fd4e887341c8a36fbedbbbdebb6b
jsanchez <jorsanchez@hotmail.fr>
2024-06-06 11:17:40 +0200

    js end of body

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,3 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            animation: {
+                'infinite-scroll': 'infinite-scroll 80s linear infinite',
+            },
+            keyframes: {
+                'infinite-scroll': {
+                    from: { transform: 'translateX(0)' },
+                    to: { transform: 'translateX(-100%)' },
+                }
+            }                    
+        },
+    },
+};
+
+
 $("input:radio[name='pricebracketstarter']").click(function(){
     $('span.startervalue').html(this.value);
 })
