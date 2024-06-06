156a65c217001a63fc1baa33b4e34186028da0cb
jsanchez <jorsanchez@hotmail.fr>
2024-06-06 15:00:33 +0200

    scroll to / header buttons

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,3 +1,17 @@
+// SCROLL TO
+$("#productButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#productSection").offset().top -50
+    }, 500);
+});
+
+$("#pricingButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#pricingSection").offset().top -50
+    }, 500);
+});
+
+// CARROUSEL
 tailwind.config = {
     theme: {
         extend: {
@@ -14,7 +28,7 @@ tailwind.config = {
     },
 };
 
-
+// DYNAMIC PRICING
 $("input:radio[name='pricebracketstarter']").click(function(){
     $('span.startervalue').html(this.value);
 })
@@ -22,4 +36,3 @@ $("input:radio[name='pricebracketstarter']").click(function(){
 $("input:radio[name='pricebracketadvanced']").click(function(){
     $('span.advancedvalue').html(this.value);
 })
-
